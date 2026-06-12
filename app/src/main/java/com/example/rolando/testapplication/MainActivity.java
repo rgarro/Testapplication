b6c290375a5997811503adfbbb11122435a876eb
Rolando <rgarro@gmail.com>
2026-06-12 17:27:07 -0600

    I foud myself alone above the raging sea

--- app/src/main/java/com/example/rolando/testapplication/MainActivity.java
+++ app/src/main/java/com/example/rolando/testapplication/MainActivity.java
@@ -3,6 +3,26 @@ package com.example.rolando.testapplication;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ *            |    |    |
+ *           )_)  )_)  )_)
+ *          )___))___))___)\
+ *         )____)____)_____)\\
+ *      _____|____|____|____\\\__
+ * -------\                   /---------
+ *     ^^^^^ ^^^^^^^^^^^^^^^^^^^^^
+ *     ^^^^      ^^^^     ^^^    ^^
+ *           ^^^^      ^^^
+ * Sir Henry Morgan is de Lord of Talamanca
+ * from the Mountains of Coronado you can see
+ * where Port Royal is, Jamaica !! Jamaica !!
+ * all the servants of his majesty must remember
+ * FALKLANDS ARE BRITISH !!! FALKLANDS ARE BRITISH !!!
+ * 
+ *
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
 public class MainActivity extends Activity {
 
     @Override

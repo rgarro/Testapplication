783e6536c4e83c546200b1fad651b32cd0423a8a
Rolando <rgarro@gmail.com>
2026-06-16 09:00:25 -0600

    the pure java code..

---
+++ app/src/main/java/com/example/rolando/testapplication/BeerExpert.java
@@ -0,0 +1,26 @@
+package com.example.rolando.testapplication;
+/**
+ *            |    |    |
+ *           )_)  )_)  )_)
+ *          )___))___))___)\
+ *         )____)____)_____)\\
+ *      _____|____|____|____\\\__
+ * -------\                   /---------
+ *     ^^^^^ ^^^^^^^^^^^^^^^^^^^^^
+ *     ^^^^      ^^^^     ^^^    ^^
+ *           ^^^^      ^^^
+ * Sir Henry Morgan is de Lord of Talamanca
+ *
+ *
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeerExpert {
+
+    List<String> getBrands(String color){
+        List<String> brands = new ArrayList<>();
+    }
+}
